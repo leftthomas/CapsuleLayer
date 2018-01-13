3ae1b49f6a6165c46b0fb3b5a77306e6ebca0e2d
leftthomas <leftthomas@qq.com>
2018-01-13 18:09:06 +0800

    change kernels

--- capsule_layer/utils.py
+++ capsule_layer/utils.py
@@ -28,58 +28,26 @@ def load_kernel(kernel_name, code, **kwargs):
 
 capsule_linear_kernels = '''
 extern "C"
-__global__ void capsule_linear_forward(${Dtype} *dst, const ${Dtype} *src, int chw, int total)
+__global__ void capsule_linear_forward(const ${Dtype}* input_data, const ${Dtype}* weight_data, ${Dtype}* output_data)
 {
-   int tx = blockIdx.x * blockDim.x + threadIdx.x;
-   if(tx >= total)
-      return;
-
-   ${Dtype} v = src[tx];
-   unsigned char flag = v >= 0;
-   mask[tx] = flag;
-   dst[tx + tx / chw * chw] = flag ? v : 0.f;
-   dst[tx + tx / chw * chw + chw] = flag ? 0.f : v;
 }
 
 extern "C"
 __global__ void capsule_linear_backward(${Dtype} *grad_input, const unsigned char *mask, const ${Dtype} *grad_output,
                                 int chw, int total)
 {
-   int tx = blockIdx.x * blockDim.x + threadIdx.x;
-   if(tx >= total)
-      return;
-
-   grad_output += tx + tx / chw * chw;
-   bool flag = mask[tx];
-   grad_input[tx] = flag ? grad_output[0] : grad_output[chw];
 }
 '''
 
 capsule_conv2d_kernels = '''
 extern "C"
-__global__ void capsule_conv2d_forward(${Dtype} *dst, unsigned char* mask, const ${Dtype} *src, int chw, int total)
+__global__ void capsule_conv2d_forward(const ${Dtype}* input_data, const ${Dtype}* weight_data, ${Dtype}* output_data)
 {
-   int tx = blockIdx.x * blockDim.x + threadIdx.x;
-   if(tx >= total)
-      return;
-
-   ${Dtype} v = src[tx];
-   unsigned char flag = v >= 0;
-   mask[tx] = flag;
-   dst[tx + tx / chw * chw] = flag ? v : 0.f;
-   dst[tx + tx / chw * chw + chw] = flag ? 0.f : v;
 }
 
 extern "C"
 __global__ void capsule_conv2d_backward(${Dtype} *grad_input, const unsigned char *mask, const ${Dtype} *grad_output,
                                 int chw, int total)
 {
-   int tx = blockIdx.x * blockDim.x + threadIdx.x;
-   if(tx >= total)
-      return;
-
-   grad_output += tx + tx / chw * chw;
-   bool flag = mask[tx];
-   grad_input[tx] = flag ? grad_output[0] : grad_output[chw];
 }
 '''
