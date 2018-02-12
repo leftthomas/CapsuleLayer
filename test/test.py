46b7501280e469b307f938e9b500a13a60e1e682
leftthomas <leftthomas@qq.com>
2018-02-12 18:28:59 +0800

    capsule_linear_input_backward have been tested, it's right

--- test/test.py
+++ test/test.py
@@ -46,10 +46,10 @@ class TestCapsuleLayer(unittest.TestCase):
 
     def test_capsule_linear(self):
         print('--------test capsule linear forward--------')
-        x_gpu = Variable(torch.randn(64, 5, 8).double().cuda(), requires_grad=True)
-        w_gpu = Variable(torch.randn(10, 16, 5, 8).double().cuda(), requires_grad=True)
-        x_cpu = x_gpu.cpu()
-        w_cpu = w_gpu.cpu()
+        x_cpu = Variable(torch.randn(64, 5, 8).double(), requires_grad=True)
+        w_cpu = Variable(torch.randn(10, 16, 5, 8).double(), requires_grad=True)
+        x_gpu = Variable(x_cpu.data.cuda(), requires_grad=True)
+        w_gpu = Variable(w_cpu.data.cuda(), requires_grad=True)
         start = time.clock()
         y_fast = CL.capsule_linear(x_gpu, w_gpu)
         print('gpu mode cost ' + str(time.clock() - start) + 's')
@@ -59,10 +59,8 @@ class TestCapsuleLayer(unittest.TestCase):
         self.assertLess((y_fast.cpu() - y_ref).data.abs().max(), 1e-9)
 
         print('--------test capsule linear backward--------')
-        go_gpu = torch.randn(y_fast.size()).double().cuda()
-        go_cpu = go_gpu.cpu()
-        x_gpu.requires_grad = True
-        w_gpu.requires_grad = True
+        go_cpu = torch.randn(y_ref.size()).double()
+        go_gpu = go_cpu.cuda()
         start = time.clock()
         y_fast.backward(go_gpu)
         print('gpu mode cost ' + str(time.clock() - start) + 's')
@@ -70,8 +68,6 @@ class TestCapsuleLayer(unittest.TestCase):
         gw_fast = w_gpu.grad.data.clone()
         self.assertTrue(gradcheck(partial(CL.capsule_linear), (x_gpu, w_gpu)))
 
-        x_cpu.requires_grad = True
-        w_cpu.requires_grad = True
         start = time.clock()
         y_ref.backward(go_cpu)
         print('cpu mode cost ' + str(time.clock() - start) + 's')
@@ -79,8 +75,8 @@ class TestCapsuleLayer(unittest.TestCase):
         gw_ref = w_cpu.grad.data.clone()
         self.assertTrue(gradcheck(partial(CL.capsule_linear), (x_cpu, w_cpu)))
 
-        self.assertLess((gx_fast.cpu() - gx_ref).data.abs().max(), 1e-9)
-        self.assertLess((gw_fast.cpu() - gw_ref).data.abs().max(), 1e-9)
+        self.assertLess((gx_fast.cpu() - gx_ref).abs().max(), 1e-9)
+        self.assertLess((gw_fast.cpu() - gw_ref).abs().max(), 1e-9)
 
     # def test_capsule_conv2d_multigpu(self):
     #     a0 = Variable(torch.randn(6, 3, 28, 32).cuda(0), requires_grad=True)
