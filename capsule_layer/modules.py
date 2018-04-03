5de52641144c0ae7eb427037d35b593731c1e39c
leftthomas <leftthomas@qq.com>
2018-04-04 00:22:34 +0800

    use xavier_uniform to init weight

--- capsule_layer/modules.py
+++ capsule_layer/modules.py
@@ -92,6 +92,8 @@ class CapsuleConv2d(nn.Module):
         self.weight = Parameter(
             torch.Tensor(out_channels // out_length, in_channels // in_length, *kernel_size, out_length, in_length))
 
+        nn.init.xavier_uniform(self.weight)
+
     def forward(self, input):
         return CL.capsule_cov2d(input, self.weight, self.stride, self.padding, self.routing_type, self.num_iterations,
                                 **self.kwargs)
@@ -160,12 +162,14 @@ class CapsuleLinear(nn.Module):
             if in_capsules is not None:
                 raise ValueError('Expected in_capsules must be None.')
             else:
-                self.weight = Parameter(torch.rand(out_capsules, out_length, in_length))
+                self.weight = Parameter(torch.Tensor(out_capsules, out_length, in_length))
         else:
             if in_capsules is None:
                 raise ValueError('Expected in_capsules must be int.')
             else:
                 self.weight = Parameter(torch.Tensor(out_capsules, in_capsules, out_length, in_length))
+
+        nn.init.xavier_uniform(self.weight)
 
     def forward(self, input):
         return CL.capsule_linear(input, self.weight, self.share_weight, self.routing_type, self.num_iterations,
