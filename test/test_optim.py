014a546545a548808c4ac09c4670b3b3436d6331
leftthomas <leftthomas@qq.com>
2018-05-11 16:18:57 +0800

    add test optim

---
+++ test/test_optim.py
@@ -0,0 +1,34 @@
+import pytest
+import torch
+from pytest import approx
+from torch.autograd import Variable
+
+from capsule_layer.functional import dynamic_routing, k_means_routing
+
+kwargs_data = {
+    'dynamic': [{'squash': squash, 'return_prob': return_prob} for squash in [True, False] for return_prob in
+                [True, False]],
+    'k_means': [{'similarity': similarity, 'squash': squash, 'return_prob': return_prob} for similarity in
+                ['dot', 'cosine', 'tonimoto', 'pearson'] for squash in [True, False] for return_prob in [True, False]]
+}
+routing_funcs = {'dynamic': dynamic_routing, 'k_means': k_means_routing}
+
+test_data = [(batch_size, out_capsules, in_capsules, out_length, routing_type, kwargs, num_iterations) for batch_size
+             in [1, 2] for out_capsules in [1, 5, 10] for in_capsules in [1, 4, 20] for out_length in [1, 8, 16] for
+             routing_type in ['dynamic', 'k_means'] for kwargs in kwargs_data[routing_type]
+             for num_iterations in [1, 4, 50]]
+
+
+@pytest.mark.parametrize('batch_size, out_capsules, in_capsules, out_length, routing_type, kwargs, num_iterations',
+                         test_data)
+def test_routing(batch_size, in_capsules, out_capsules, out_length, routing_type, kwargs, num_iterations):
+    x = torch.randn(batch_size, out_capsules, in_capsules, out_length).double()
+    if kwargs['return_prob']:
+        y_cpu, prob_cpu = routing_funcs[routing_type](Variable(x), num_iterations, **kwargs)
+        y_cuda, prob_cuda = routing_funcs[routing_type](Variable(x.cuda()), num_iterations, **kwargs)
+        assert y_cuda.cpu().data.view(-1).tolist() == approx(y_cpu.data.view(-1).tolist())
+        assert prob_cuda.cpu().data.view(-1).tolist() == approx(prob_cpu.data.view(-1).tolist())
+    else:
+        y_cpu = routing_funcs[routing_type](Variable(x), num_iterations, **kwargs)
+        y_cuda = routing_funcs[routing_type](Variable(x.cuda()), num_iterations, **kwargs)
+        assert y_cuda.cpu().data.view(-1).tolist() == approx(y_cpu.data.view(-1).tolist())
